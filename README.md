b7c8246e8857d85bbb8bf87c11536aa2dcb7c3e2
João Belo <joao.belo@bloq.it>
2024-03-12 16:56:40 +0000

    clean project

--- README.md
+++ README.md
@@ -6,29 +6,20 @@ Currently, only single file upload is supported.
 
 ## Inputs
 
-| Name | Description | Default |
-| --- | --- | --- |
-| `aws-access-key-id` | (Required) Your AWS access key ID. | |
-| `aws-secret-access-key` | (Required) Your AWS secret access key. | |
-| `aws-region` | (Required) Region where the bucket is located. | |
-| `aws-bucket` | (Required) S3 bucket to upload files. | |
-| `file-path` | (Required) Path of the file to upload, eg `./myfile.txt` | |
-| `destination-dir` | Directory on the bucket to upload files. If you don't want to apply anything, specify `/`. | 32 random alphanumeric characters |
-| `bucket-root` | Root directory on the bucket to upload files. Useful for separating objects in buckets that are not related to this action. If you don't want to apply anything, specify `/`. | `artifacts` |
-| `output-file-url` | Add the URL of the file to the output of this action. | `false` |
-| `content-type` | Specify the contents of the 'Content-type' header when downloading the file, eg `image/png`. | |
-| `output-qr-url` | Generate a QR code image for the URL of the file and add the URL of the image to the output of this action. Useful for mobile devices. | `false` |
-| `qr-width` | QR code image width pixels. Specify `100` to `1000`. | `120` |
-| `public` | If `false` is specified, [ACL](https://docs.aws.amazon.com/AmazonS3/latest/userguide/acl-overview.html#canned-acl) is set to 'private' and presigned URL is used. Basically should be `false` for private buckets. | `false` |
-| `expire` | Expiration seconds for presigned URL. Specify `0` to `86400`(1 week). | `86400` |
+| Name | Description | 
+| --- | --- | 
+| `aws-access-key-id` | (Required) Your AWS access key ID. | 
+| `aws-secret-access-key` | (Required) Your AWS secret access key. | 
+| `aws-region` | (Required) Region where the bucket is located. | 
+| `aws-bucket` | (Required) S3 bucket to upload files. | 
+| `destination-dir` | Directory on the bucket to upload files | 
+| `metadata` | metadata in json format | 
 
 ## Outputs
 
 | Name | Description |
 | --- | --- |
 | `result` | Result of this action. `success` or `failure` is set. |
-| `file-url` | URL of the uploaded file. |
-| `qr-url` | URL of the generated QR code image. |
 
 ## Usage
 
@@ -41,63 +32,18 @@ Currently, only single file upload is supported.
     aws-secret-access-key: ${{ secrets.AWS_SECRET_ACCESS_KEY }}
     aws-region: 'ap-northeast-1'
     aws-bucket: ${{ secrets.AWS_BUCKET }}
-    file-path: './myfile.txt'
+    file-path: 'myfile.txt'
+    destination-dir: 'folder'
+    metadata:  |
+      {
+        "key1": "value1",
+        "key2": "value2",
+        "key3": "value3"
+      }
 ```
 
-In this example, `myfile.txt` is stored in `artifacts/<32 random characters>/myfile.txt` on the bucket.
-Specify `destination-dir` input or `bucket-root` input to change the destination.
-
-### URL of the uploaded file
-
-Use `file-url` output.
-
-```yaml
-- uses: bloqit/s3-upload-action@v2
-  id: upload # specify some ID for use in subsequent steps
-  with:
-    aws-access-key-id: ${{ secrets.AWS_ACCESS_KEY_ID }}
-    aws-secret-access-key: ${{ secrets.AWS_SECRET_ACCESS_KEY }}
-    aws-region: 'ap-northeast-1'
-    aws-bucket: ${{ secrets.AWS_BUCKET }}
-    file-path: './myfile.txt'
-    output-file-url: 'true' # specify true
-- name: Show URL
-  run: echo '${{ steps.upload.outputs.file-url }}' # use this output
-```
-
-When uploading an image and displaying it on a browser, specify `image/png` etc. for `content-type` input.
-For Android apk file, you can install it on your device by specifying `application/vnd.android.package-archive`.
-
-### URL of the generated QR code image
-
-Use `qr-url` output.
-
-```yaml
-- uses: bloqit/s3-upload-action@v2
-  id: upload # specify some ID for use in subsequent steps
-  with:
-    aws-access-key-id: ${{ secrets.AWS_ACCESS_KEY_ID }}
-    aws-secret-access-key: ${{ secrets.AWS_SECRET_ACCESS_KEY }}
-    aws-region: 'ap-northeast-1'
-    aws-bucket: ${{ secrets.AWS_BUCKET }}
-    file-path: './myfile.txt'
-    output-qr-url: 'true' # specify true
-- name: Show URL
-  run: echo '${{ steps.upload.outputs.qr-url }}' # use this output
-```
-
-Specify `qr-width` input to change the size of the QR code image.
-
-QR code image sample:
-
-![image](doc/qr.png)
-
-## Publish file to public
-
-If `false` is specified for the `public` input, ACL is set to 'private' and presigned URL is used.
-However, the expiration date of the presigned URL is up to one week (It can also be shortened by specifying the `expire` input).
-
-If you don't want to set the expiration date, you can get the normal URL without authentication by specifying `true` for the `public` input (Bucket side settings are also required to make it public).
+In this example, `myfile.txt` is stored in `folder/myfile.txt` on the bucket.
+Specify `destination-dir` input to change the destination.
 
 ## License
 
--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ const core = require('@actions/core');
 const aws = require('aws-sdk');
 const fs = require('fs');
 const path = require('path');
-const qr = require('qrcode');
 const crc32 = require('buffer-crc32');
 
 const NODE_ENV = process.env['NODE_ENV'];
@@ -19,17 +18,9 @@ if (NODE_ENV != 'local') {
     awsSecretAccessKey: core.getInput('aws-secret-access-key', { required: true }),
     awsRegion: core.getInput('aws-region', { required: true }),
     awsBucket: core.getInput('aws-bucket', { required: true }),
-    filePath: core.getInput('file-path', { required: true }),
     destinationDir: core.getInput('destination-dir'),
-    bucketRoot: core.getInput('bucket-root'),
-    outputFileUrl: core.getInput('output-file-url'),
-    contentType: core.getInput('content-type'),
-    outputQrUrl: core.getInput('output-qr-url'),
-    qrWidth: core.getInput('qr-width'),
-    public: core.getInput('public'),
-    expire: core.getInput('expire'),
-    version: core.getInput('version') || null,
-    lastCommitSha: core.getInput('last-commit-sha') || null,
+    filePath: core.getInput('file-path', { required: true }),
+    metadata: core.getInput('metadata')
   };
 } else {
   input = {
@@ -37,17 +28,9 @@ if (NODE_ENV != 'local') {
     awsSecretAccessKey: AWS_SECRET_ACCESS_KEY,
     awsRegion: 'ap-northeast-1',
     awsBucket: AWS_BUCKET,
-    filePath: './README.md',
     destinationDir: '',
-    bucketRoot: '/',
-    outputFileUrl: 'true',
-    contentType: '',
-    outputQrUrl: 'true',
-    qrWidth: '120',
-    public: 'false',
-    expire: '180',
-    version: null,
-    lastCommitSha: null,
+    filePath: './README.md',
+    metadata: ''
   };
 }
 
@@ -57,131 +40,25 @@ aws.config.update({
   region: input.awsRegion,
 });
 
-const s3 = new aws.S3({signatureVersion: 'v4'});
+const s3 = new aws.S3({ signatureVersion: 'v4' });
 
 async function run(input) {
 
-  const expire = parseInt(input.expire);
-  if (!expire | expire < 0 | 604800 < expire) {
-    throw new Error('"expire" input should be a number between 0 and 604800.');
-  }
-
-  const qrWidth = parseInt(input.qrWidth);
-  if (!qrWidth | qrWidth < 100 | 1000 < qrWidth) {
-    throw new Error('"qr-width" input should be a number between 100 and 1000.');
-  }
-
-  let bucketRoot = input.bucketRoot;
-  if (bucketRoot) {
-    if (bucketRoot.startsWith('/')) {
-      bucketRoot = bucketRoot.slice(1);
-    }
-    if (bucketRoot && !bucketRoot.endsWith('/')) {
-      bucketRoot = bucketRoot + '/'
-    }
-  } else {
-    bucketRoot = 'artifacts/'; // Do not use the default value of input to match the behavior with destinationDir
-  }
-
-  let destinationDir = input.destinationDir;
-  if (destinationDir) {
-    if (destinationDir.startsWith('/')) {
-      destinationDir = destinationDir.slice(1);
-    }
-    if (destinationDir && !destinationDir.endsWith('/')) {
-      destinationDir = destinationDir + '/'
-    }
-  } else {
-    destinationDir = getRandomStr(32) + '/';
-  }
-
-  const fileKey = bucketRoot + destinationDir + path.basename(input.filePath);
-
-  let acl;
-  if (input.public == 'true') {
-    acl = 'public-read';
-  } else {
-    acl = 'private';
-  }
-
+  const fileKey = input.destinationDir + '/' + path.basename(input.filePath);
   const file = fs.readFileSync(input.filePath);
   const checksum = crc32(file).toString('base64');
 
   let params = {
     Bucket: input.awsBucket,
     Key: fileKey,
-    ContentType: input.contentType,
     Body: file,
-    ACL: acl,
+    ACL: 'private',
     ChecksumCRC32: checksum,
     ChecksumAlgorithm: 'CRC32',
+    Metadata: JSON.parse(input.metadata)
   };
 
-  let metadata = {}
-
-  if (input.version)
-  { 
-    metadata["version"] = input.version;
-  }
-
-  if (input.lastCommitSha)
-  { 
-    metadata["lastcommitsha"] = input.lastCommitSha;
-  }
-
-  if (input.version || input.lastCommitSha)
-  { 
-    params["Metadata"] = metadata;
-  }
-
   await s3.putObject(params).promise();
-
-  let fileUrl;
-  if (input.outputFileUrl == 'true' || input.outputQrUrl == 'true') {
-    if (input.public == 'true') {
-      fileUrl = `https://${input.awsBucket}.s3-${input.awsRegion}.amazonaws.com/${fileKey}`;
-    } else {
-      params = {
-        Bucket: input.awsBucket,
-        Key: fileKey,
-        Expires: expire,
-      };
-      fileUrl = await s3.getSignedUrlPromise('getObject', params);
-    }
-    if (input.outputFileUrl == 'true') {
-      core.setOutput('file-url', fileUrl);
-    }
-  }
-
-  if (input.outputQrUrl != 'true') return;
-
-  const qrKey = bucketRoot + destinationDir + 'qr.png';
-  const tmpQrFile = './s3-upload-action-qr.png';
-
-  await qr.toFile(tmpQrFile, fileUrl, { width: qrWidth })
-
-  params = {
-    Bucket: input.awsBucket,
-    Key: qrKey,
-    ContentType: 'image/png', // Required to display as an image in the browser
-    Body: fs.readFileSync(tmpQrFile),
-    ACL: acl,
-  };
-  await s3.putObject(params).promise();
-  fs.unlinkSync(tmpQrFile);
-
-  let qrUrl;
-  if (input.public == 'true') {
-    qrUrl = `https://${input.awsBucket}.s3-${input.awsRegion}.amazonaws.com/${qrKey}`;
-  } else {
-    params = {
-      Bucket: input.awsBucket,
-      Key: qrKey,
-      Expires: expire,
-    };
-    qrUrl = await s3.getSignedUrlPromise('getObject', params);
-  }
-  core.setOutput('qr-url', qrUrl);
 }
 
 run(input)
@@ -192,12 +69,3 @@ run(input)
     core.setOutput('result', 'failure');
     core.setFailed(error.message);
   });
-
-function getRandomStr(length) {
-  const c = 'abcdefghijklmnopqrstuvwxyzABCDEFGHJKLMNOPQRSTUVWXYZ0123456789';
-  let r = '';
-  for(let i = 0; i < length; i++) {
-    r += c[Math.floor(Math.random() * c.length)];
-  }
-  return r;
-}
